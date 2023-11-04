e4731bb90f270855efd77e780663b07ebe0e2a6b
unknown <obinaligoodness@gmail.com>
2023-11-04 10:32:10 +0100

    responsiveness

--- app/components/GetMovies.tsx
+++ app/components/GetMovies.tsx
@@ -84,10 +84,10 @@ const Index: React.FC = () => {
       
     </div>
   ) :<div className="w-full h-700 bg-gray-900 text-white">
-    <div className='w-full h-20  bg-gray-800'>
-      <h1 className='text-red-600 text-3xl ml-4 pt-4'>wowmoviez</h1>
+    <div className='w-full md:h-20 h-14  bg-gray-800'>
+      <h1 className='text-lg ml-2 text-red-600 md:text-3xl md:ml-4 pt-4'>wowmoviez</h1>
     </div>
-    <h1 className=' text-lg ml-6 text-red-600  pt-16 md:ml-0 md:text-3xl md:justify-center flex '> <span className='text-white mr-2 '>welcome to </span> wowmoviez</h1>
+    <h1 className=' text-lg ml-10 text-red-600  pt-16 md:ml-0 md:text-3xl md:justify-center flex '> <span className='text-white mr-2 '>welcome to </span> wowmoviez</h1>
     <div className="md:justify-center md:flex-row md:items-center flex">
   <input type='text' value={query} onChange={(e) => {setquery(e.target.value); handlesearch()}} placeholder='Search movies...' className="h-10 w-42 ml-10 md:ml-4 md:h-10 md:w-96  md:justify-center mt-6 rounded-md text-black"></input>
   {/* <button onClick={handlesearch} className="bg-yellow-400 h-10 w-20 rounded-full mt-7 ">Search</button> */}
